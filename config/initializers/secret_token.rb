d65d70c3d8871bcb8795d99629cba5ccaee33b2c
Sudheer K. Parasker <sparasker@gmail.com>
2013-10-06 19:35:27 -0500

    Initialize Respository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mobloyalty::Application.config.secret_key_base = '5e6c3c65cf8a989a6cbce4fc1b3b025f128ed60781a5351405787038fa10be8b32b0cb2ea29eb6d2ca1f30c7b56cd4f8df6f14ed1812f0e53219c762f7912745'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mobloyalty::Application.config.session_store :cookie_store, key: '_mobloyalty_session'
