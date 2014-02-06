aac96fd80557e5e2be30ecc977d2e66e33b4c2cc
Sudheer K. Parasker <sparasker@gmail.com>
2014-02-05 23:20:14 -0600

    mails not working in prod

--- app/controllers/user_mailer.rb
+++ app/controllers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ActionMailer::Base
 def password_reset(user)
   @user = user
-  mail :to => user.email, :subject => "Password Reset"
+	headers["Reply-to"] = "admin@mobloyalty.com"
+  headers["Return-Path"] = "admin@mobloyalty.com" 
+   mail(:to => user.email, :subject => "Password Reset", :from => "admin@mobloyalty.com") 
 end
 end
