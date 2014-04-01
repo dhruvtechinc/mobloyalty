f26f417d1f804594ec8c0a481cf6234fbf796e67
Dhruv Technologies Inc <dhruv.tech.inc@gmail.com>
2014-03-31 21:02:34 -0500

    Delete 20140312015937_change_membership_to_integer.rb

--- db/migrate/20140312015937_change_membership_to_integer.rb
+++
@@ -1,6 +0,0 @@
-class ChangeMembershipToInteger < ActiveRecord::Migration
-  def change
-  	remove_column :rewards, :membership_id, :string
-  	add_column :rewards, :membership_id, :integer
-  end
-end
