d7da3274d42954a851fbc50faf1c890d71d9a47f
Dhruv Technologies Inc <dhruv.tech.inc@gmail.com>
2014-03-31 21:01:51 -0500

    Delete 20140206023256_remove_new_column.rb

--- db/migrate/20140206023256_remove_new_column.rb
+++
@@ -1,6 +0,0 @@
-class RemoveNewColumn < ActiveRecord::Migration
-  def change
-  	remove_column :users, :password_reset_sent_ar, :datetime
-  	add_column :users, :password_reset_sent_at, :datetime
-  end
-end
